1a79d329ad90c40819ce9c729d634fd45760c910
Manish <vermadevelopmentgroup@gmail.com>
2024-12-10 10:13:56 +0530

    Initial commit

---
+++ lib/screen/ambulanceDriver/DriverModel.dart
@@ -0,0 +1,50 @@
+class DriverModel {
+  String name;
+  String experience;
+  String? email;
+  String mobileNo;
+  String vehicleNo;
+  String address;
+  String status;
+  String driverId;
+
+  // Constructor
+  DriverModel({
+    required this.name,
+    required this.experience,
+    this.email,
+    required this.mobileNo,
+    required this.status,
+    required this.vehicleNo,
+    required this.address,
+    required this.driverId,
+  });
+
+  // Factory method to create a DriverModel from a map (e.g., from Firestore)
+  factory DriverModel.fromMap(Map<String, dynamic> map) {
+    return DriverModel(
+      name: map['name'] ?? '',
+      status: map['status'] ?? '',
+      experience: map['experience'] ?? '',
+      email: map['email'],
+      mobileNo: map['mobileNo'] ?? '',
+      vehicleNo: map['vehicleNo'] ?? '',
+      address: map['address'] ?? '',
+      driverId: map['driverId'] ?? '',
+    );
+  }
+
+  // Method to convert a DriverModel to a map (e.g., for Firestore)
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'experience': experience,
+      'email': email,
+      'mobileNo': mobileNo,
+      'status': status,
+      'vehicleNo': vehicleNo,
+      'address': address,
+      'driverId': driverId,
+    };
+  }
+}
---
+++ lib/screen/patientManagementScreen/AppointmentModel.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AppointmenModel {
+  final String id;
+  final String? userId;
+  final String doctorName;
+  final String category;
+  final String date;
+  final String time;
+  final String verification;
+   String status;
+
+  AppointmenModel(
+      {required this.id,
+      required this.userId,
+      required this.verification,
+      required this.doctorName,
+      required this.category,
+      required this.date,
+      required this.time,
+      required this.status});
+
+
+  factory AppointmenModel.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return AppointmenModel(
+      id: doc.id,
+      doctorName: data['doctorName'] ?? '',
+      userId: data['userId'] ?? '',
+      date: data['date'] ?? '',
+      verification: data['verification'] ?? '',
+      time: data['time'] ?? '',
+      category: data['category'] ?? '',
+      status: data['status'] ?? 'Pending',
+    );
+  }
+  // Convert Appointment instance to Firestore document
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'userId': userId,
+      'doctorName': doctorName,
+      'verification': verification,
+      'date': date,
+      'time': time,
+      'category': category,
+      'status': status,
+    };
+  }
+}
